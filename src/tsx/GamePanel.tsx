282d4c441d40977c653b5f748085a772c18ba560
bonbud-macryg <oarsman_phlox.0s@icloud.com>
2025-04-19 13:12:50 +0100

    Remove unnecessary awaits

--- src/tsx/GamePanel.tsx
+++ src/tsx/GamePanel.tsx
@@ -39,52 +39,52 @@ export function GamePanel () {
 
   const resignOnClick = async () => {
     const gameID = displayGame.gameID
-    await pokeAction(urbit, resignPoke(gameID))
+     pokeAction(urbit, resignPoke(gameID))
   }
 
   const offerDrawOnClick = async () => {
     const gameID = displayGame.gameID
-    await pokeAction(urbit, offerDrawPoke(gameID))
+     pokeAction(urbit, offerDrawPoke(gameID))
   }
 
   const acceptDrawOnClick = async () => {
     const gameID = displayGame.gameID
-    await pokeAction(urbit, acceptDrawPoke(gameID))
+     pokeAction(urbit, acceptDrawPoke(gameID))
   }
 
   const declineDrawOnClick = async () => {
     const gameID = displayGame.gameID
-    await pokeAction(urbit, declineDrawPoke(gameID))
+     pokeAction(urbit, declineDrawPoke(gameID))
   }
 
   const revokeDrawOnClick = async () => {
     const gameID = displayGame.gameID
-    await pokeAction(urbit, revokeDrawPoke(gameID))
+     pokeAction(urbit, revokeDrawPoke(gameID))
   }
 
   const claimSpecialDrawOnClick = async () => {
     const gameID = displayGame.gameID
-    await pokeAction(urbit, claimSpecialDrawPoke(gameID))
+     pokeAction(urbit, claimSpecialDrawPoke(gameID))
   }
 
   const requestUndoOnClick = async () => {
     const gameID = displayGame.gameID
-    await pokeAction(urbit, requestUndoPoke(gameID))
+     pokeAction(urbit, requestUndoPoke(gameID))
   }
 
   const acceptUndoOnClick = async () => {
     const gameID = displayGame.gameID
-    await pokeAction(urbit, acceptUndoPoke(gameID))
+     pokeAction(urbit, acceptUndoPoke(gameID))
   }
 
   const declineUndoOnClick = async () => {
     const gameID = displayGame.gameID
-    await pokeAction(urbit, declineUndoPoke(gameID))
+     pokeAction(urbit, declineUndoPoke(gameID))
   }
 
   const revokeUndoOnClick = async () => {
     const gameID = displayGame.gameID
-    await pokeAction(urbit, revokeUndoPoke(gameID))
+     pokeAction(urbit, revokeUndoPoke(gameID))
   }
 
   const moveOpacity = (index: number) => {
