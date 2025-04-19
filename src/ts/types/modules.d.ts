3e94daeb009a5ce0b7cff6331482448d48327e32
bonbud-macryg <oarsman_phlox.0s@icloud.com>
2025-04-19 14:03:02 +0100

    Swap module type declarations

--- src/ts/types/modules.d.ts
+++ src/ts/types/modules.d.ts
@@ -1,8 +1,8 @@
+declare module 'urbit-ob' {
+  function isValidPatp(ship: string): boolean;
+}
+
 declare module '*.svg' {
   const content: string
   export default content
-}
-
-declare module 'urbit-ob' {
-  function isValidPatp(ship: string): boolean;
 }
