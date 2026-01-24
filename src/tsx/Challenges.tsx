7724f18703bc51f1a76d1da9f490d556a9e95579
bonbud-macryg <oarsman_phlox.0s@icloud.com>
2026-01-24 14:47:44 +0000

    Challenges: remove todo

--- src/tsx/Challenges.tsx
+++ src/tsx/Challenges.tsx
@@ -241,7 +241,6 @@ export function Challenges () {
         }
       </ul>
       {/* New Challenge popup */}
-      {/* XX getTally runs on each chance to the description field */}
       <Popup open={modalOpen} className='challenges-popup' onClose={resetChallengeInterface}>
         <div className='new-challenge-container col'>
           <p className='new-challenge-header'>New Challenge</p>
